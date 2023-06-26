d51ad612bae52b3c144cb0b022b9efe7c14bd1ed
Maciej Brzostowski <mbrzostowski@atlassian.com>
2023-06-26 15:40:18 +0200

    refactor: add method to clear cookies each new test

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,10 @@ def log_in(request, driver):
     This fixture will login user.
     Use @pytest.mark.no_login to prevent this behaviour
     """
+    driver.delete_all_cookies()
     if request.node.get_closest_marker("no_login"):
         return
     login_page = LoginPage(driver).load_from_url(config.site_login_page)
-    return login_page.log_in(config.user_email, config.email_password)
+    page = login_page.log_in(config.user_email, config.email_password)
+    login_page.wait_for_page_to_load()
+    return page
